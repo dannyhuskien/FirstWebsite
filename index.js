e2c639ca78a01428840e9d54c597f4edd3a91006
MJ Kubba and Syed Nabeel <pair+mj+syed@allstate.com>
2016-06-20 14:50:29 -0500

    adding things to dev

--- index.js
+++ index.js
@@ -20,22 +20,27 @@ app.post('/dog', function(req, res){
 });
 
 app.get('/add/:x/:y', function(req, res){
+  // const y = req.params.y *1;
+  // const x = req.params.x *1;
+  // ret = x+y
+  // res.send({payload: x+y});
   const y = req.params.y *1;
   const x = req.params.x *1;
   ret = x+y
-  res.send({payload: x+y});
+  res.render('sum', {x: x, y: y, sum: x + y});
 });
 
 app.get('/fact/:x', function(req, res){
   const x = req.params.x *1;
   result = factorial(x);
-  res.send({payload: result});
+  res.render('factorial',{x: x, o: result});
 });
 
 app.get('/sum_square_evens/:x', function(req, res){
   const x  = req.params.x.split(',')
   const result = x.reduce((a, n) => n%2 ? a+0 : a+(n*n), 0);
-  res.send({payload: result});
+  //res.send({payload: result});
+  res.render('sum_square_even', {x: x, o: result});
 });
 
 function factorial(num)
